af699b9b5814d1a432e6a7221ad6c02349bf9a39
rwoods1227 <43154167+rwoods1227@users.noreply.github.com>
2018-10-26 03:25:17 -0400

    Update analyzeTextFile.cpp

--- src/analyzeTextFile.cpp
+++ src/analyzeTextFile.cpp
@@ -130,45 +130,4 @@ on =1;
   ifstr.close();
 
 
-
-
-
-
-}
-
-#include <iostream>
-#include <string>
-#include "analyzeTextFile.h"
-
-using namespace std;
-
-int main(int argc, char *argv[])
-{
-
-    //Question 1: Analyze a text file and output the total number of lines as well as the total number of displayable characters including whitespace.
-    string fileName = "adder32.txt";
-    unsigned int lineCount = 0;
-    unsigned int characterCount = 0;
-
-    // if filename entered through the command line
-    if (argc > 1)
-    {
-        fileName = argv[1];
-    }
-    // if not, ask for filename
-    else
-    {
-      cout << "Enter a file name to analyze: ";
-      cin >> fileName;
-
-      //fileName = "../sampleTextFile.txt";
-    }
-
-
-    analyzeTextFile(fileName, lineCount, characterCount);
-    //  cout << endl;
-    //cout << lineCount << " lines" << endl;
-    //66cout << characterCount << " characters" << endl;
-
-    return 0;
 }
