161f3405f7c72f07a5963bc5ed13620da8c6c2ae
rwoods1227 <43154167+rwoods1227@users.noreply.github.com>
2018-10-26 05:15:44 -0400

    Update analyzeTextFile.cpp

--- src/analyzeTextFile.cpp
+++ src/analyzeTextFile.cpp
@@ -17,9 +17,8 @@ int nodes = 0;
 int on = 0;
 int n =0;
 string names = ".names";	
-//const char* c_stringTest = names.c_str();	
-//char const*
-std::map<std::string,int>::iterator it;
+	
+//std::map<std::string,int>::iterator it;
 
 //intialize map
 map<std::string, int> adder32;
@@ -27,11 +26,10 @@ map<std::string, int> adder32;
   // read file, if not exists cout a warning
   stringstream str;
   ifstream ifstr(fileName);
+	
   if (ifstr.is_open())
  {
    string s;
-	  //string s
-//cout << 1 << endl;
 	  
    while (getline(ifstr,s, ' '))
  {
@@ -83,10 +81,11 @@ else {
 }
 
 }
-
+//various checks	   
 //cout << s.length() << endl;	   
-//cout << s.compare(names) << endl;	   
-// checks for node
+//cout << s.compare(names) << endl;
+	   
+// checks for node	   
 if (s.compare(names) == 0)
    {
 nodes = nodes +1;
@@ -94,52 +93,13 @@ cout << "nodes = " << nodes << endl;
 on = 1;
    }
 
-//old code for stringstream
-  // str  << s;
  }
 
- /*
- char c;
-  while (str.get(c))
-   {
-     //    cout <<c;
-    if (str.peek() == '\t')
-     {
-       //  cout << '\t';
-       //   lineCount = lineCount +1;
-      characterCount = characterCount + 1;
-     }
-    //  else if (str.peek() == '\t')
-    //  {
-    //      cout << '\t';
-    //      characterCount = characterCount + 2;
-    //    }
-    else if (str.peek() == ' ')
-     {
-       //   cout << ' ';
-      characterCount = characterCount + 1;
-     }
-    else
-     {
-	characterCount = characterCount + 1;
-     }
-    }
-    */
  }
-
-
  //error output
   else {
     cerr << "No such file exists" << endl;
  }
-
-
-  //cout << characterCount << endl;
-    //   cout << lineCount << endl;
-
-
  // close file
   ifstr.close();
-
-
 }
